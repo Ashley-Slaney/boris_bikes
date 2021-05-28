975a47acaa46a87abaf542fdd625079c458c350e
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-05-28 11:53:42 +0100

    Minor changes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,8 @@
 require_relative 'bike'
 
 class DockingStation
-<<<<<<< HEAD
-  attr_reader :bike
-
-=======
-  # attr_reader :dock 
+  attr_reader :bike 
   
->>>>>>> aadb5d5aac75cce18e3492fa202478aecb2b124c
   def release_bike
     Bike.new
   end
